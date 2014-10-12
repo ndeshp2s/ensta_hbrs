c5191426271a541732ef7ac98c090d2e9c8e7480
niranjan <niranjan@lenovo-G710.(none)>
2014-10-12 03:31:22 +0200

    reverted lost changes ..partial

---
+++ semantic_knowledge_base/semantic_costmap_global/src/inflation_semantic_costmap.cpp
@@ -0,0 +1,19 @@
+#include "semantic_costmap_global/semantic_costmap.h"
+#include "semantic_costmap_global/inflation_semantic_costmap.h"
+
+InflationSemanticCostmap:: InflationSemanticCostmap(){
+
+}
+
+InflationSemanticCostmap::~ InflationSemanticCostmap(){
+
+}
+
+bool InflationSemanticCostmap::buildSemanticCostmap(){
+
+	return false; // not implemented yet
+
+}
+
+
+
